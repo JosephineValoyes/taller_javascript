f55519c73b74c06e7141bc67ede55a74521a58b0
Josefina Valoyes <93234678+JosephineValoyes@users.noreply.github.com>
2024-06-04 15:56:55 -0500

    Add files via upload

---
+++ taller_javascript/ejercicio8.js
@@ -0,0 +1,3 @@
+/*Considerando que 𝑎 = 4, 𝑏 = 7, 𝑐 = 2𝑏, 𝑥 = 𝑎 , 𝑦 = 3𝑥, 𝑢 = 6, 𝑤 = 10, 𝑧 = 2𝑤; 
+crear un algoritmo con JavaScript que realice y muestre el resultado en consola de 
+las siguientes operaciones algebraica*/
---
+++ taller_javascript/main.js
@@ -0,0 +1,11 @@
+/*De un operario se conoce su sueldo y años de antigüedad. Se pide crear un 
+programa que lea los datos de entrada y muestre en consola el valor booleano 
+de las siguientes comparaciones:
+i. ¿El sueldo es inferior a $2000 US y su antigüedad es igual o superior a 
+10 años?
+ii. ¿El sueldo es inferior a $2000 US o su antigüedad es menor a 10 años?
+iii. ¿El sueldo es mayor o igual a $2000 US? */
+
+let sueldoMy = 10;
+let sueldoMn = 9;
+console.log(sueldoMn)
